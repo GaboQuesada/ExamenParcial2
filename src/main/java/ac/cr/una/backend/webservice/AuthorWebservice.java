473a167b580643432135f49a3b1357311c06ffca
Gabo Quesada <gabrielquesadasanchez@gmail.com>
2017-02-01 16:44:09 -0600

    merge

--- src/main/java/ac/cr/una/backend/webservice/AuthorWebservice.java
+++ src/main/java/ac/cr/una/backend/webservice/AuthorWebservice.java
@@ -1,8 +1,14 @@
 package ac.cr.una.backend.webservice;
 
 import ac.cr.una.backend.dao.AuthorContactDAO;
+import ac.cr.una.backend.dao.AuthorContactDAOImpl;
 import ac.cr.una.backend.dao.AuthorDAO;
+import ac.cr.una.backend.dao.AuthorDAOImpl;
+import ac.cr.una.backend.model.Author;
+import ac.cr.una.backend.model.AuthorContact;
+import ac.cr.una.backend.model.BookType;
 import ac.cr.una.backend.service.AuthorService;
+import ac.cr.una.backend.service.AuthorServiceImpl;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -27,12 +33,62 @@ public class AuthorWebservice {
     private AuthorDAO authorDAO;
     private AuthorContactDAO authorContactDAO;
     private AuthorService authorService;
-    
+
     @Context
     private UriInfo context;
 
     public AuthorWebservice() {
     }
+
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public AuthorContact createAuthor(AuthorContact authorContact) {
+
+        authorContactDAO = new AuthorContactDAOImpl();
+        authorService = new AuthorServiceImpl(authorContactDAO);
+
+        authorContact = authorService.save(authorContact);
+
+        return authorContact;
+    }
+
+    @DELETE
+    @Path("/")
+    public boolean deleteALL() {
+        boolean result;
+        authorContactDAO = new AuthorContactDAOImpl();
+        authorService = new AuthorServiceImpl(authorContactDAO);
+
+        result = authorService.deleteAll();
+
+        return result;
+    }
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Author getAuthorbyName(@PathParam("name") String name) {
+        Author author = null;
+        authorDAO = new AuthorDAOImpl();
+        authorService = new AuthorServiceImpl(authorContactDAO);
+
+        author = authorService.findByName(name);
+
+        return author;
+    }
     
     
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<AuthorContact> getAllAuthors() {
+        List<AuthorContact> lista = null;
+        authorContactDAO = new AuthorContactDAOImpl();
+        authorService = new AuthorServiceImpl(authorContactDAO);
+
+        lista = authorService.findAll();
+        return lista;
+    }
 }
